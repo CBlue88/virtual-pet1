b13507eee0e4f787f5468699aa0cae037d225294
Chanel Williams <chnlwllms8@gmail.com>
2017-01-23 10:25:56 -0500

    first commit

---
+++ Virtual Pet/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Pet
+
+{
+    public class Virtual_Pet
+    {
+
+        private string nameOfPet;
+        private string genderOfPet;
+        private string colorOfPet;
+        //------------------------------
+        private string dietOfPet;  // I am going to design a method to feed the monkey
+        private string foodoption; // I may need to have a way to store the selected item
+        string[] myFoods = new string[] { "apple", "banana", "roots", "leaves" };
+
+        // --------------------------------------------------------------------------
+        string[] playforpet = { "wrestle", "catch", "groom", "swing" };//going to make a method to play with monkey
+        private string playOfPet;//to activate method?
+        private string playoption; // I may need to store the option picked from play to verify or at least print out for user, to know if it worked.
+                                   // I may need to 
+
+
+        //These are my contructors for my object
+        //This is the input that my user would pass to these variables
+        //The "this" keyword enables the user to call that variable(get access) to input data
+        //Change of plans, I wanted to incorporate other animal types but this would cause too much focus on too many things
+
+
+        //This will enable my user to put in a name for their monkey
+        //This will enable my user to put in a gender
+        //In all constructor will build for the virutal pet: Monkey, MOMO, Female, AKA Momo the monkey 
+        //The "this" keyword enables the user to call that variable(get access) to input data
+
+        //PROPERTIES
+
+        public string name
+        {
+            get { return nameOfPet;}
+            set { nameOfPet = value;}
+        { 
+        
+                
+
+        public string color
+        {
+            get { return colorOfPet; }
+            set { colorOfPet = value; }
+        }
+
+        public string gender
+        {
+            get { return genderOfPet; }
+            set { genderOfPet = value; }
+        }
+
+
+        public Virtual_Pet(string name, string gender, string color)
+        {
+
+            this.nameOfPet = name;
+            this.colorOfPet = color;
+            this.genderOfPet = gender;
+        }
+
+
+
+
+
+
+
+    }
+
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Monkey Madness!");
+            Console.WriteLine("You are going to have your very own Funky little Monkey to take care of!");
+            Console.WriteLine("After you give your Monkey a name, gender, and color, your monkey will prompt you for care");
+            Console.WriteLine("You are will be given options to take care of our monkey, make good decisions, and good luck!");
+            //THIS IS THE EXPLAINATION AND PROMPT DETAILING MONKEY MADNESS
+            Virtual_Pet monkey = new Virtual_Pet();
+
+            Console.WriteLine("What is your Monkey's name?");
+            monkey.nameOfPet = Console.ReadLine();
+            Console.WriteLine("What gender is your monkey?");
+            monkey.genderOfPet = Console.ReadLine();
+            Console.WriteLine("What is the color of your monkey?");
+            monkey.colorOfPet = Console.ReadLine();
+            Console.WriteLine("Cool! You have a" + "" + monkey.colorOfPet + "monkey!");
+
+            Console.WriteLine("Now lets get started!");
+            Console.WriteLine(monkey.nameOfPet + "needs to be taken care of!");
+            Random random = new Random();//Random function
+            string[] myStrings = new string[] { "hunger", "sleepy", "bored", };
+            string[] myFoods = new string[] { "apple", "banana", "roots", "leaves" };
+            //----------------------------------------------INSERT LOOP HERE??
+            for (int n = 0; n < 1; n++)
+            {
+                int rnd = random.Next(0, myStrings.Length);
+                string s = myStrings[rnd];
+                Console.WriteLine("-> {0}", s);
+            }
+
+            if (myStrings.Equals("hungry"))
+            {
+                Console.WriteLine(monkey.nameOfPet + ""+ "is hungry");  // Make the monkey's name a variable here. 
+                Console.WriteLine("Would you like to feed" +""+ monkey.nameOfPet?); //Factor in her and him for gender selection
+                Console.ReadLine();
+
+                if (Console.ReadLine().Equals("yes"))
+                {
+                    food();
+                }
+
+                else
+                {
+                    Console.WriteLine("Your monkey is hungry!");
+                }
+            }
+
+            if (myStrings.Equals("bored"))
+            {
+                Console.WriteLine("Momo is" + "bored"); //Make the monkey's name a variable here
+                Console.WriteLine("Would you like to play with her?");
+                if (Console.ReadLine().Equals("yes"))
+                {
+                    bored();
+                }
+                else
+                {
+                    Console.WriteLine("Your monkey is bored!");
+                }
+            }
+
+            if (myStrings.Equals("sleepy"))
+            {
+                Console.WriteLine("Momo is" + "sleepy"); //Make the monkey's name a variable here
+                Console.WriteLine("Would you like to put her to bed?");
+                if (Console.ReadLine().Equals("yes"))
+                {
+                    sleep();
+                }
+                else
+                {
+                    Console.WriteLine("Your monkey needs sleep!");
+                }
+
+
+
+
+
+
+
+            }
+    }
+        // METHODS START HERE
+        static void food()
+        {
+            string[] myFoods = new string[] { "apple", "banana", "roots", "leaves" };
+            myFoods.ToList().ForEach(Console.WriteLine);
+        }
+
+        static void bored()
+        {
+            string[] playforpet = { "wrestle", "catch", "groom", "swing" };
+            playforpet.ToList().ForEach(Console.WriteLine);
+        }
+        static void sleep()
+        {
+            Console.WriteLine("You monkey is asleep and will be well rested!");
+        }
+
+    }
